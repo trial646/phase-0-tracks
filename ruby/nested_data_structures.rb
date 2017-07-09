a5b58b69d0bc508bf70b3dfdb63c6f5d68bb504f
Trial646 <storesandthings@zoho.com>
2017-07-09 14:08:26 -0400

    updated retreival technique to get deep nest elements

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,16 +1,13 @@
 classroom = {
- 'desk1': ['ruler', 'markers'],
- 'desk2': ['folder', 'compass'],
- 'desk3': [],
- 'desk4' => {'pencilcase':
+ 'desk1'=> ['ruler', 'markers'],
+ 'desk2'=> ['folder', 'compass'],
+ 'desk3'=> [],
+ 'desk4' => {'pencilcase'=>
    ['pencils','erasers']
  }
 }
 
 p classroom
 
-# tried to return 'erasers', nested in an array that is the value of a hash that is nested in another hash!
-#p classroom['desk4']['pencilcase']['erasers']
-# this returns error message
-#classroom['desk4']['pencilcase'][1]
-# this returns nil
+classroom['desk4']['pencilcase'][1]
+
