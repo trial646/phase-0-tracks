44162b9fc8c37cd4e856a17be77fed2e64a588bb
Trial646 <storesandthings@zoho.com>
2017-07-09 06:15:40 -0400

    first steps in 5.5 release 0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+=begin
+Pseudo code for release 0
+Make a string of all vowels
+call a method that finds all of the vowels in the name
+find or make a method that converts each vowel in the name to the next vowel letter
+Make a string of all consonants
+call a method that finds all consonants in the name
+use the same method to advance every consonent to become the next consonant
+
+
+Original instructions: Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+=end
+
