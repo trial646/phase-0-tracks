6fba795226c649a07f6d34f6294935149d07eeb9
Trial646 <storesandthings@zoho.com>
2017-07-13 16:28:42 -0400

    release 1 of testing

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -4,24 +4,31 @@
 # multiply two integers and return an integer
 
 # RSpec
-# 'describe' block for each group of tests  
+# 'describe' block for each group of tests
   # 'it' block for each individual test
     # expect(<YOUR CODE>).to eq <RESULT>
 
+# our calculator should ...
+# add two integers and return an integer
+# subtract two integers and return an integer
+# multiply two integers and return an integer
+# RSpec
+# 'describe' block for each group of tests
+  # 'it' block for each individual test
+    # expect(<YOUR CODE>).to eq <RESULT>
 require_relative 'calculator'
-
 describe Calculator do
   let(:calculator) { Calculator.new }
-
   it "adds two integers" do
     expect(calculator.add(3,4)).to eq 7
   end
-
   it "subtracts two integers" do
     expect(calculator.subtract(7,1)).to eq 6
   end
-
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
+  end
+  it "divides two integers" do
+    expect(calculator.divide(12,4)).to eq 3
   end
 end
