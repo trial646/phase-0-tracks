1ac26f359b6ff0790535017fbb3bbe88e5236ebd
Trial646 <storesandthings@zoho.com>
2017-07-13 23:37:31 -0400

    opening steps in testing exercises

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,10 @@
+
+class TodoList
+  def initialize(items)
+    puts "#{items}"
+    #@list = []
+
+  end
+
+end
+new_list = TodoList.new("mow the lawn")
--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,3 +1,5 @@
+require_relative 'todo_list.rb'
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
