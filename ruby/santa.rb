e1fc3f5c3c93eca2efbb08a3cca2c029c1feaea5
Trial646 <storesandthings@zoho.com>
2017-07-14 00:02:36 -0400

    opening steps for 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,7 +2,7 @@ def speak
   p "Ho, ho, ho! Haaaappy holidays!"
 end
 
-def eat_milk_and_cookies
-  cookie_type = gets.chomp
-  p "That was a good #{cookie_type}!"
+def eat_milk_and_cookies(cookie_type)
+  p "That was a good #{cookie_type} cookie!"
 end
+
