b334f7d588056decb1deefbf3123247df23d9faf
Trial646 <storesandthings@zoho.com>
2017-07-20 17:22:36 -0400

    completed calculator exercise

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,14 +1,42 @@
-class Calculator
-  def add(x,y)
-    x + y
+class TodoList
+  def initialize(array)
+    @array = array
   end
-  def subtract(x,y)
-    x - y
+
+  def get_items
+    p @array
   end
-  def multiply(x,y)
-    x * y
+
+  def add_item(new_item)
+    @new_item = new_item
+    @array << new_item
   end
-  def divide(x,y)
-    x / y
+
+  def delete_item(removed_item)
+    @removed_item = removed_item
+    @array.delete(removed_item)
   end
+
+   def get_item(index)
+     @array.fetch(index)
+   end
+
 end
+
+todo_list = TodoList.new(["do the dishes", "mow the lawn"])
+todo_list.get_items
+todo_list
+todo_list.add_item("mop")
+todo_list.delete_item("do the dishes")
+p todo_list
+p todo_list.get_item(0)
+
+
+
+
+
+# Program creates an array containing two strings
+#and puts it inside of the class on initialization
+
+# array = ["string1", "string2"]
+# p array
