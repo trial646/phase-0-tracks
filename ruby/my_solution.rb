5690a6699e33f7ecd8a5475517b5526121d413a2
Trial646 <storesandthings@zoho.com>
2017-07-21 22:36:51 -0400

    completed steps through release 5 or so

--- ruby/my_solution.rb
+++ ruby/my_solution.rb
@@ -4,27 +4,31 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# links the current working file with external file in order to pass
+# data from one file to the other.
+# require loads files that already exist inside ruby.
 require_relative 'state_data'
 
 class VirusPredictor
-
+ # declaring a set of attributes that attaches to a new class instance when it is created
+ # and when a new class instance is created these attributes
+ # aplly to that class instance.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+ # predicting outcomes based on a formula that takes the desnity, polulatin size and state as parameters.
+ # predicting speed of spead
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
-
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
+
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
@@ -41,7 +45,7 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -82,6 +86,14 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
+# STATE_DATA.each do |state_name, pop_info|
+#   puts state_name
+#   puts pop_info[:population_density]
+# end
 
+
+STATE_DATA.each do |state_name, pop_info|
+  VirusPredictor.new(state_name, pop_info[:population_density], pop_info[:population]).virus_effects
+end
 #=======================================================================
 # Reflection Section
