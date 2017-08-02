38886b011c0bd2057655e8c128d7216919430586
Trial646 <storesandthings@zoho.com>
2017-08-02 17:21:08 -0400

    psuedo coded a puppy class

---
+++ ruby/puppy_G_Bears.rb
@@ -0,0 +1,15 @@
+#Puppy Class
+#Species : canine
+
+#Characteristics
+# Eyes: 2
+# Social: true
+# Tail-type: varies
+# Legs: 4
+# Fur: True
+# Tongue hangs out: Occassionally
+
+#Behavior -
+#Barks
+#Drools
+#chews stuff that isnt food
